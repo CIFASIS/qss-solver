d6427f39d57bf8cb4eab7178544ae7f03aae19a4
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2017-05-29 15:05:52 -0300

    Added graph_profile files (removed from common folder).

---
+++ util/graph_profile.cpp
@@ -0,0 +1,97 @@
+/*****************************************************************************
+
+ This file is part of QSS Solver.
+
+ QSS Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QSS Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#include <stdlib.h>
+
+#include "graph_profile.h"
+
+
+double
+GRP_nodeWeight (GRP_graphProfile g, GRP_EdgeType type)
+{
+  if (type == GRP_CONT)
+    {
+      return (g->state->contEdgeWeight);
+    }
+  else if (type == GRP_DSC)
+    {
+      return (g->state->dscEdgeWeight);
+    }
+  else
+    {
+      return (g->state->virtEdgeWeight);
+    }
+  return (0);
+}
+
+GRP_graphProfile
+GRP_GraphProfile ()
+{
+  GRP_graphProfile p = malloc (sizeof(*p));
+  p->ops = GRP_GraphProfileOps ();
+  p->state = GRP_GraphProfileState ();
+  return (p);
+}
+
+void
+GRP_freeGraphProfile (GRP_graphProfile g)
+{
+  GRP_freeGraphProfileOps (g->ops);
+  GRP_freeGraphProfileState (g->state);
+  free (g);
+}
+
+GRP_graphProfileOps
+GRP_GraphProfileOps ()
+{
+  GRP_graphProfileOps p = malloc (sizeof(*p));
+  p->weight = GRP_nodeWeight;
+  return (p);
+}
+
+void
+GRP_freeGraphProfileOps (GRP_graphProfileOps ops)
+{
+  free (ops);
+}
+
+GRP_graphProfileState
+GRP_GraphProfileState ()
+{
+  GRP_graphProfileState p = malloc (sizeof(*p));
+  p->states = 0;
+  p->events = 0;
+  p->contEdgeWeight = 10;
+  p->dscEdgeWeight = 10;
+  p->virtEdgeWeight = 1;
+  return (p);
+}
+
+void
+GRP_freeGraphProfileState (GRP_graphProfileState state)
+{
+  free (state);
+}
+
+double
+GRP_Weight (GRP_graphProfile g, GRP_EdgeType type)
+{
+  return (g->ops->weight(g, type));
+}
+
---
+++ util/graph_profile.h
@@ -0,0 +1,130 @@
+/*****************************************************************************
+
+    This file is part of QSS Solver.
+
+    QSS Solver is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    QSS Solver is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
+#ifndef GRAPH_GRAPH_PROFILE_H_
+#define GRAPH_GRAPH_PROFILE_H_
+
+typedef enum
+{
+  GRP_CONT,
+  GRP_DSC,
+  GRP_VIRT
+} GRP_EdgeType;
+
+/**
+ *
+ */
+typedef struct GRP_graphProfileOps_ *GRP_graphProfileOps;
+
+/**
+ *
+ */
+typedef struct GRP_graphProfileState_ *GRP_graphProfileState;
+
+/**
+ *
+ */
+typedef struct GRP_graphProfile_ *GRP_graphProfile;
+
+/**
+ *
+ * @param
+ * @param
+ * @param
+ */
+typedef double
+(*GRP_graphProfileWeight) (GRP_graphProfile, GRP_EdgeType);
+
+/**
+ *
+ */
+struct GRP_graphProfileOps_
+{
+  GRP_graphProfileWeight weight; //!<
+};
+
+/**
+ *
+ */
+struct GRP_graphProfileState_
+{
+  int states; //!<
+  int events; //!<
+  double contEdgeWeight;
+  double dscEdgeWeight;
+  double virtEdgeWeight;
+};
+
+/**
+ *
+ */
+struct GRP_graphProfile_
+{
+  GRP_graphProfileOps ops; //!<
+  GRP_graphProfileState state; //!<
+};
+
+/**
+ *
+ * @return
+ */
+
+GRP_graphProfile
+GRP_GraphProfile ();
+
+/**
+ *
+ * @param g
+ */
+void
+GRP_freeGraphProfile (GRP_graphProfile g);
+
+/**
+ *
+ * @return
+ */
+GRP_graphProfileOps
+GRP_GraphProfileOps ();
+
+/**
+ *
+ * @param ops
+ */
+void
+GRP_freeGraphProfileOps (GRP_graphProfileOps ops);
+
+/**
+ *
+ * @return
+ */
+GRP_graphProfileState
+GRP_GraphProfileState ();
+
+/**
+ *
+ * @param state
+ */
+void
+GRP_freeGraphProfileState (GRP_graphProfileState state);
+
+double
+GRP_Weight (GRP_graphProfile g, GRP_EdgeType type);
+
+
+#endif /* GRAPH_GRAPH_PROFILE_H_ */
