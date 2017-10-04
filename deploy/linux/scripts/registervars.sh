2dcde9d387bb601cede3eaeaa6a6e01089311f59
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2017-10-04 14:41:39 -0300

    Modified environment variables for Linux.

--- deploy/linux/scripts/registervars.sh
+++ deploy/linux/scripts/registervars.sh
@@ -17,22 +17,21 @@
 #       VERSION: 3.1
 #===================================================================================
 
-
 cd ..
 echo Export environment variables...
 sed -i.bak '/MMOC/d' ~/.bashrc
 echo 'export MMOC_PATH='`pwd`'' >> ~/.bashrc
-echo 'export MMOC_ENGINE=$MMOC_PATH/src/engine/' >> ~/.bashrc
-echo 'export MMOC_LIBS=$MMOC_PATH/src/libs' >> ~/.bashrc
+echo 'export MMOC_SRC=$MMOC_PATH/src' >> ~/.bashrc
+echo 'export MMOC_DOC=$MMOC_PATH/doc' >> ~/.bashrc
+echo 'export MMOC_LIBS=$MMOC_PATH/lib' >> ~/.bashrc
 echo 'export MMOC_BUILD=$MMOC_PATH/build' >> ~/.bashrc
 echo 'export MMOC_MODELS=$MMOC_PATH/models' >> ~/.bashrc
 echo 'export MMOC_OUTPUT=$MMOC_PATH/output' >> ~/.bashrc
 echo 'export MMOC_BIN=$MMOC_PATH/bin' >> ~/.bashrc
-echo 'export MMOC_TESTS=$MMOC_PATH/testsuite' >> ~/.bashrc
 echo 'export MMOC_PACKAGES=$MMOC_PATH/packages' >> ~/.bashrc
-echo 'export MMOC_LIBRARIES=$MMOC_PATH/usr/libs' >> ~/.bashrc
-echo 'export MMOC_INCLUDE=$MMOC_PATH/usr/include' >> ~/.bashrc
-echo 'export MMOC_SRC=$MMOC_PATH/src' >> ~/.bashrc
-echo 'export MMOC_DOC=$MMOC_PATH/doc' >> ~/.bashrc
+echo 'export MMOC_LIBRARIES=$MMOC_PATH/lib' >> ~/.bashrc
+echo 'export MMOC_TESTS=$MMOC_SRC/test-suite' >> ~/.bashrc
+echo 'export MMOC_ENGINE=$MMOC_SRC/engine/' >> ~/.bashrc
+echo 'export MMOC_INCLUDE=$MMOC_SRC/usr/include' >> ~/.bashrc
 cd bin
 echo Done.
