819a828e393636c04a224e71c6a9becca7ff3e03
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2017-10-05 15:18:55 -0300

    Modified Windows builder.

--- deploy/windows/build.sh
+++ deploy/windows/build.sh
@@ -20,12 +20,13 @@
 
 cd ../../
 echo "Retrieving latest from Git";
-git pull
+./deploy/common/repos.sh pull
 head ./doc/version.major -c 4 > vm
 git rev-list --count HEAD > rvn
 cat vm rvn > version
 REV=`cat rvn`
-./deploy/linux/setRevision.sh ./deploy/windows/qss-solver.ini $REV 
+cat ./deploy/windows/qss-solver.ini.in > ./deploy/windows/qss-solver.ini
+./deploy/common/setRevision.sh ./deploy/windows/qss-solver.ini $REV 
 VER=`cat version`
 echo Building Windows Package version $VER
 echo Creating directories
@@ -35,51 +36,62 @@ mkdir tmp
 mkdir tmp-win-installer
 mkdir ./tmp-win-installer/qss-solver
 mkdir ./tmp-win-installer/qss-solver/bin
+mkdir ./tmp-win-installer/qss-solver/src
 mkdir ./tmp-win-installer/qss-solver/build
 mkdir ./tmp-win-installer/qss-solver/output
-mkdir ./tmp-win-installer/qss-solver/usr
-mkdir ./tmp-win-installer/qss-solver/usr/libs
+mkdir ./tmp-win-installer/qss-solver/lib
 echo Done.
 
 echo Exporting files
-git checkout-index --prefix=tmp/ -a
+./deploy/common/repos.sh deploy tmp 
 cp -r ./tmp/deploy/windows/export/*  ./tmp-win-installer/
 cp -r ./tmp/models ./tmp-win-installer/qss-solver/
-cp -r ./tmp/src ./tmp-win-installer/qss-solver/
-cp -r ./tmp/usr/* ./tmp-win-installer/qss-solver/usr/
 cp -r ./tmp/packages ./tmp-win-installer/qss-solver/
 cp -r ./tmp/doc ./tmp-win-installer/qss-solver/
-cp doc/COPYING ./tmp-win-installer/qss-solver/
-cp doc/INSTALL ./tmp-win-installer/qss-solver/
-cp doc/README.txt ./tmp-win-installer/qss-solver/
-cp doc/ChangeLog ./tmp-win-installer/qss-solver/
+cp -r ./tmp/src/engine ./tmp-win-installer/qss-solver/src/
+cp -r ./tmp/src/mmoc ./tmp-win-installer/qss-solver/src/
+cp -r ./tmp/src/gui ./tmp-win-installer/qss-solver/src/
+cp -r ./tmp/src/interfaces ./tmp-win-installer/qss-solver/src/
+cp -r ./tmp/src/usr ./tmp-win-installer/qss-solver/src/
+cp LICENSE ./tmp-win-installer/qss-solver/
+cp INSTALL ./tmp-win-installer/qss-solver/
+cp README.md ./tmp-win-installer/qss-solver/
+cp CHANGELOG ./tmp-win-installer/qss-solver/
 cp deploy/images/integrator.svg ./tmp-win-installer/qss-solver/bin
 cp deploy/windows/qss-solver.ini ./tmp-win-installer/qss-solver/bin
 cp deploy/windows/qss-solver.gpr ./tmp-win-installer
+# Code cleanup.
+rm -rf ./deploy/windows/qss-solver.ini
+rm -rf ./tmp_deb/opt/qss-solver/src/engine/3rd-party
+rm -rf ./tmp_deb/opt/qss-solver/src/engine/usr/obj
+rm -rf ./tmp_deb/opt/qss-solver/src/mmoc/usr/obj
+rm -rf ./tmp_deb/opt/qss-solver/src/mmoc/usr/bin
+rm -rf ./tmp_deb/opt/qss-solver/src/mmoc/usr/share
+rm -rf ./tmp_deb/opt/qss-solver/src/mmoc/usr/lib
+rm -rf ./tmp_deb/opt/qss-solver/src/mmoc/usr/libexec
+rm -rf ./tmp_deb/opt/qss-solver/src/usr/src/*.o
+rm -rf ./tmp_deb/opt/qss-solver/src/usr/lib
+rm -rf ./tmp_deb/opt/qss-solver/src/interfaces/sbml/usr
+rm -rf ./tmp_deb/opt/qss-solver/src/gui/usr
 echo Done.
 echo Building binaries
 cd src
-make OS=win32 
+make clean
+make 
 cd ..
 echo Done.
 echo Copying executables files 
-cd bin
-cp qss-solver.exe ../tmp-win-installer/qss-solver/bin/
-cp mmoc.exe ../tmp-win-installer/qss-solver/bin/
-cp translate-sbml.exe ../tmp-win-installer/qss-solver/bin/
+cp src/gui/usr/bin/qss-solver.exe tmp-win-installer/qss-solver/bin/
+cp src/mmoc/usr/bin/mmoc.exe tmp-win-installer/qss-solver/bin/
+cp src/interfaces/sbml/usr/bin/translate-sbml.exe tmp-win-installer/qss-solver/bin/
 echo Done.
 echo Copying libraries
-cd ../src/libs
-cp *.a ../../tmp-win-installer/qss-solver/src/libs/
-cp ../../usr/libs/*.a ../../tmp-win-installer/qss-solver/usr/libs/
-cd ../../tmp-win-installer
-mv ./qss-solver/bin/*.a ./qss-solver/src/libs/
+cp src/engine/usr/lib/*.a tmp-win-installer/qss-solver/lib/
+cp src/usr/lib/*.a tmp-win-installer/qss-solver/lib/
+rm -rf rvn
+rm -rf version
+rm -rf vm
+cd tmp-win-installer
+mv ./qss-solver/bin/*.a ./qss-solver/lib/
 echo Done.
-echo Creating installer
-gibuild qss-solver.gpr
-cp ./Output/qss-solver-install.exe ../
-cd ..
-rm rvn vm version
-rm -rf tmp-win-installer 
-mv qss-solver-install.exe deploy/windows/
-echo Done.
+echo Build finished, now create the installer.
