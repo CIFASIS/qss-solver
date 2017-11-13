89da4aae243b1fde57de18c5d6574725bef4c756
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2017-11-13 18:05:14 -0300

    chg: Added WR_MODEL_FULL section to writer.

--- generator/generator_utils.h
+++ generator/generator_utils.h
@@ -31,7 +31,7 @@ using namespace std;
 /**
  *
  */
-#define SECTIONS 	71
+#define SECTIONS 	72
 /**
  *
  */
@@ -92,6 +92,7 @@ typedef enum
   WR_INIT_VARS,          //!< WR_INIT_VARS
   WR_MODEL_SIMPLE,       //!< WR_MODEL_SIMPLE
   WR_MODEL_GENERIC,      //!< WR_MODEL_GENERIC
+  WR_MODEL_FULL,         //!< WR_MODEL_FULL
   WR_MODEL_DEPS_SIMPLE,  //!< WR_MODEL_DEPS_SIMPLE
   WR_MODEL_DEPS_GENERIC, //!< WR_MODEL_DEPS_GENERIC
   WR_ZC_SIMPLE,          //!< WR_ZC_SIMPLE
