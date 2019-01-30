f50eb06e8e26d292f2d5ece8b45db7e2a87d361c
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2019-01-30 13:05:17 -0300

    WIP

--- deps/graph/graph.h
+++ deps/graph/graph.h
@@ -1,24 +1,24 @@
 /*****************************************************************************
 
-    This file is part of Modelica C Compiler.
+ This file is part of QSS Solver.
 
-    Modelica C Compiler is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ QSS Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    Modelica C Compiler is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ QSS Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Modelica C Compiler.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
 
-******************************************************************************/
+ ******************************************************************************/
 
-#ifndef GRAPH_DEFINITION_
-#define GRAPH_DEFINITION_
+#ifndef DEPS_GRAPH_H_
+#define DEPS_GRAPH_H_
 
 #include <iostream>
 #include <utility>
