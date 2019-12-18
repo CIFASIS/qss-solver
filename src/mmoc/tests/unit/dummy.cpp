1100bd5f3edb0ba1d78619a8d352431ee7fc7c39
Joaquin <joaquin.f.fernandez@gmail.com>
2019-12-18 14:39:19 -0300

    Just use one dummy test for now.

--- src/mmoc/tests/unit/dummy.cpp
+++ src/mmoc/tests/unit/dummy.cpp
@@ -4,9 +4,6 @@
 using std::string;
 
 const char *actualValTrue = "hello gtest";
-const char *actualValFalse = "hello world";
 const char *expectVal = "hello gtest";
 
 TEST(StrCompare, CStrEqual) { EXPECT_STREQ(expectVal, actualValTrue); }
-
-TEST(StrCompare, CStrNotEqual) { EXPECT_STREQ(expectVal, actualValFalse); }
