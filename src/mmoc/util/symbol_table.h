e74276f18726bffbfd07a658c8c3764d0559c80f
Joaquin <joaquin.f.fernandez@gmail.com>
2020-03-04 20:32:39 -0300

    Remove comments, make methods virtual for mocking.

--- src/mmoc/util/symbol_table.h
+++ src/mmoc/util/symbol_table.h
@@ -39,9 +39,6 @@ namespace Util {
 
 class VarSymbolTable;
 
-/**
- *
- */
 class Variable {
   public:
   Variable();
@@ -51,83 +48,83 @@ class Variable {
 
   typedef enum { State, Algebraic, NotAssigned } RealType;
 
-  inline void setRealType(RealType type) { _realType = type; };
-  inline AST_TypePrefix typePrefix() { return _tp; };
-  inline AST_Comment comment() { return _comm; };
-  inline void setComment(AST_Comment c) { _comm = c; };
-  inline AST_Modification modification() { return _m; };
-  inline void setModification(AST_Modification m)
+  virtual void setRealType(RealType type) { _realType = type; };
+  virtual AST_TypePrefix typePrefix() { return _tp; };
+  virtual AST_Comment comment() { return _comm; };
+  virtual void setComment(AST_Comment c) { _comm = c; };
+  virtual AST_Modification modification() { return _m; };
+  virtual void setModification(AST_Modification m)
   {
     _m = m;
     processModification();
   };
-  inline Type type() { return _t; };
-  inline void setType(Type t) { _t = t; };
-  inline void setParameter()
+  virtual Type type() { return _t; };
+  virtual void setType(Type t) { _t = t; };
+  virtual void setParameter()
   {
     _tp = TP_PARAMETER;
     unsetStartEach();
   };
-  inline bool isParameter() const { return _tp & TP_PARAMETER; };
-  inline bool isDiscrete() const { return (_tp & TP_DISCRETE) || _discrete; };
-  inline bool builtIn() const { return _builtin; };
-  inline void setDiscrete()
+  virtual bool isParameter() const { return _tp & TP_PARAMETER; };
+  virtual bool isDiscrete() const { return (_tp & TP_DISCRETE) || _discrete; };
+  virtual bool builtIn() const { return _builtin; };
+  virtual void setDiscrete()
   {
     _discrete = true;
     unsetAssignment();
   };
-  inline void setBuiltIn() { _builtin = true; };
-  inline bool isConstant() const { return _tp & TP_CONSTANT; };
-  inline bool isInput() const { return _tp & TP_INPUT; };
-  inline bool isOutput() const { return _tp & TP_OUTPUT; };
-  inline bool isForType() const { return _tp & TP_FOR; };
-  inline bool isEqType() const { return _tp & TP_EQ; };
-  inline bool isState() const { return _realType == State; };
-  inline bool isString() const { return _t->getType() == TYSTRING; };
-  inline void setState() { unsetAssignment(); };
-  inline bool isUnknown() { return _unknown; };
-  inline void setUnknown() { _unknown = true; };
-  inline bool isTime() { return _name.compare("time") == 0; };
-  inline bool isAlgebraic() const { return _realType == Algebraic; };
-  inline void setAlgebraic()
+  virtual void setBuiltIn() { _builtin = true; };
+  virtual bool isConstant() const { return _tp & TP_CONSTANT; };
+  virtual bool isInput() const { return _tp & TP_INPUT; };
+  virtual bool isOutput() const { return _tp & TP_OUTPUT; };
+  virtual bool isForType() const { return _tp & TP_FOR; };
+  virtual bool isEqType() const { return _tp & TP_EQ; };
+  virtual bool isState() const { return _realType == State; };
+  virtual bool isString() const { return _t->getType() == TYSTRING; };
+  virtual void setState() { unsetAssignment(); };
+  virtual bool isUnknown() { return _unknown; };
+  virtual void setUnknown() { _unknown = true; };
+  virtual bool isTime() { return _name.compare("time") == 0; };
+  virtual bool isAlgebraic() const { return _realType == Algebraic; };
+  virtual void setAlgebraic()
   {
     unsetAssignment();
     unsetStartEach();
   };
-  inline void setValue(int val) { _value = val; };
-  inline int value() { return _value; };
-  unsigned int size();
-  inline bool hasAssignment() { return _hasAssigment; };
-  inline bool hasStartModifier() { return _hasStart; };
-  inline bool hasEachModifier() { return _hasEach; };
-  inline void setEachModifier(bool each) { _hasEach = each; };
-  inline string name() const { return _name; };
-  inline void setName(string n) { _name = n; };
-  inline AST_Expression exp() { return _exp; };
-  inline bool isArray() { return _isArray; };
-  inline bool isScalar() { return !isArray(); };
+  virtual void setValue(int val) { _value = val; };
+  virtual int value() { return _value; };
+  virtual unsigned int size();
+  virtual bool hasAssignment() { return _hasAssigment; };
+  virtual bool hasStartModifier() { return _hasStart; };
+  virtual bool hasEachModifier() { return _hasEach; };
+  virtual void setEachModifier(bool each) { _hasEach = each; };
+  virtual string name() const { return _name; };
+  virtual void setName(string n) { _name = n; };
+  virtual AST_Expression exp() { return _exp; };
+  virtual bool isArray() { return _isArray; };
+  virtual bool isScalar() { return !isArray(); };
   friend ostream& operator<<(ostream& os, const Variable& e);
-  inline unsigned int size(int dim) const { return _size[dim]; };
-  unsigned int rowSize(unsigned int dim) const;
-  inline unsigned int dimensions() const { return _size.size(); };
-  std::string declaration(std::string prefix = "");
-  std::string initialization(const VarSymbolTable& symbols);
-  inline bool hasOffset() { return _hasOffset; };
-  inline void setOffset(int offset)
+  virtual unsigned int size(int dim) const { return _size[dim]; };
+  virtual unsigned int rowSize(unsigned int dim) const;
+  virtual unsigned int dimensions() const { return _size.size(); };
+  virtual std::string declaration(std::string prefix = "");
+  virtual std::string initialization(const VarSymbolTable& symbols);
+  virtual bool hasOffset() { return _hasOffset; };
+  virtual void setOffset(int offset)
   {
     _offset = offset;
     _hasOffset = true;
   };
-  inline int offset() const { return _offset; };
-  inline bool isModelVar() const { return isState() || isDiscrete() || isAlgebraic() || isParameter() || isEqType() || isOutput(); };
-  std::string print() const;
+  virtual int offset() const { return _offset; };
+  virtual bool isModelVar() const { return isState() || isDiscrete() || isAlgebraic() || isParameter() || isEqType() || isOutput(); };
+  virtual std::string print() const;
   friend std::ostream& operator<<(std::ostream& out, const Variable& v);
 
   private:
   std::string variableExpression(const VarSymbolTable& symbols);
   void processModification();
   void unsetAssignment() { _hasAssigment = false; };
-  inline void unsetStartEach()
+  virtual void unsetStartEach()
   {
     _hasEach = false;
     _hasStart = false;
@@ -153,28 +150,23 @@ class Variable {
   RealType _realType;
 };
 
-/**
- *
- */
 class TypeSymbolTable : public ModelTable<TypeName, Type> {
   public:
   TypeSymbolTable();
 };
 
-/**
- *
- */
 class VarSymbolTable : public ModelTable<VarName, Variable> {
   public:
   VarSymbolTable();
   ~VarSymbolTable() = default;
-  void initialize(TypeSymbolTable tst);
-  void insert(VarName n, Variable vi);
-  inline bool parameters() { return _parameters; };
+  virtual void initialize(TypeSymbolTable tst);
+  virtual void insert(VarName n, Variable vi);
+  virtual bool parameters() { return _parameters; };
 
   private:
   bool _parameters;
 };
 }  // namespace Util
 }  // namespace MicroModelica
+
 #endif /* SYMBOL_TABLE_H_ */
