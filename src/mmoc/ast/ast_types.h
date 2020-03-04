4c72bd1e533f9b27114159cba4b132ab417474f8
Joaquin <joaquin.f.fernandez@gmail.com>
2020-03-04 20:30:06 -0300

    Remove dead comments and typos.

--- src/mmoc/ast/ast_types.h
+++ src/mmoc/ast/ast_types.h
@@ -27,581 +27,201 @@
 
 #include "../util/macros.h"
 
-/**
- *
- */
 #define MAKE_SPACE \
   for (int __i = 0; __i < block; __i++) ret << " ";
-/**
- *
- */
 #define BEGIN_BLOCK block += 2;
-/**
- *
- */
 #define END_BLOCK block -= 2;
-/**
- *
- */
+#define current_element(it) (*it)
+#define foreach(it, list) for (it = list->begin(); it != list->end(); it++)
+#define foreachReverse(it, list) for (it = list->rbegin(); it != list->rend(); it++)
+
 extern int block;
-/**
- *
- */
 class MCC_Parser;
 
 using namespace std;
 
-/**
- *
- */
 typedef std::set<string> PreSet_;
-/**
- *
- */
 typedef PreSet_ *PreSet;
-/**
- *
- */
 typedef bool AST_Boolean;
-/**
- *
- */
 typedef string *AST_String;
-/**
- *
- */
 typedef int AST_Integer;
-/**
- *
- */
 typedef double AST_Real;
-/**
- *
- */
 typedef string AST_Type;
-/**
- *
- */
 typedef int AST_TypePrefix;
-/**
- *
- */
 typedef int AST_ClassPrefix;
 
-/**
- *
- */
 DEFINE_TYPE(AST_Node);
-/**
- *
- */
 DEFINE_TYPE(AST_Argument);
-/**
- *
- */
 DEFINE_TYPE(AST_Argument_Modification);
-/**
- *
- */
 DEFINE_TYPE(AST_Argument_Redeclaration);
-/**
- *
- */
 DEFINE_TYPE(AST_Class);
-/**
- *
- */
 DEFINE_TYPE(AST_Comment);
-/**
- *
- */
 DEFINE_TYPE(AST_Element_Component);
-/**
- *
- */
 DEFINE_TYPE(AST_Composition);
-/**
- *
- */
 DEFINE_TYPE(AST_CompositionElement);
-/**
- *
- */
 DEFINE_TYPE(AST_CompositionEqsAlgs);
-/**
- *
- */
 DEFINE_TYPE(AST_Declaration);
-/**
- *
- */
 DEFINE_TYPE(AST_Element);
-/**
- *
- */
 DEFINE_TYPE(AST_Element_Component);
-/**
- *
- */
 DEFINE_TYPE(AST_Element_ClassWrapper);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_Call);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_Connect);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_Else);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_Equality);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_For);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_If);
-/**
- *
- */
 DEFINE_TYPE(AST_Equation_When);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_BinOp);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Boolean);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_BooleanNot);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Brace);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Call);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_CallArgs);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Colon);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_ComponentReference);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Derivative);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_End);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_If);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_If_ElseIf);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Integer);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_NamedArgument);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Null);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Output);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Range);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_Real);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_String);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_UMinus);
-/**
- *
- */
 DEFINE_TYPE(AST_Expression_ArrayIndex);
-/**
- *
- */
 DEFINE_TYPE(AST_External_Function_Call);
-/**
- *
- */
 DEFINE_TYPE(AST_Element_ExtendsClause);
-/**
- *
- */
 DEFINE_TYPE(AST_Element_ImportClause);
-/**
- *
- */
 DEFINE_TYPE(AST_ForIndex);
-/**
- *
- */
 DEFINE_TYPE(AST_ImportClause);
-/**
- *
- */
 DEFINE_TYPE(AST_Modification);
-/**
- *
- */
 DEFINE_TYPE(AST_Modification_Assign);
-/**
- *
- */
 DEFINE_TYPE(AST_Modification_Class);
-/**
- *
- */
 DEFINE_TYPE(AST_Modification_Equal);
-/**
- *
- */
 DEFINE_TYPE(AST_ShortClassExp);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_Break);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_Else);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_Return);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_When);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_While);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_For);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_If);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_Assign);
-/**
- *
- */
 DEFINE_TYPE(AST_Statement_OutputAssigment);
-/**
- *
- */
 DEFINE_TYPE(AST_StoredDefinition);
-/**
- *
- */
+
 DEFINE_LIST(AST_Argument);
-/**
- *
- */
 DEFINE_LIST(AST_Class);
-/**
- *
- */
 DEFINE_LIST(AST_Element_Component);
-/**
- *
- */
 DEFINE_LIST(AST_CompositionElement);
-/**
- *
- */
 DEFINE_LIST(AST_Declaration);
-/**
- *
- */
 DEFINE_LIST(AST_Element);
-/**
- *
- */
 DEFINE_LIST(AST_Equation);
-/**
- *
- */
 DEFINE_LIST(AST_Equation_Else);
-/**
- *
- */
 DEFINE_LIST(AST_Expression);
-/**
- *
- */
 DEFINE_LIST(AST_ExpressionList);
-/**
- *
- */
 DEFINE_LIST(AST_Expression_ComponentReference);
-/**
- *
- */
 DEFINE_LIST(AST_ForIndex);
-/**
- *
- */
 DEFINE_LIST(AST_Statement);
-/**
- *
- */
 DEFINE_LIST(AST_Statement_Else);
-/**
- *
- */
 DEFINE_LIST(AST_String);
-/**
- *
- */
 DEFINE_LIST(AST_Integer);
 
-/**
- *
- */
 enum BinOpType {
-  BINOPOR,         //!< BINOPOR
-  BINOPAND,        //!< BINOPAND
-  BINOPLOWER,      //!< BINOPLOWER
-  BINOPLOWEREQ,    //!< BINOPLOWEREQ
-  BINOPGREATER,    //!< BINOPGREATER
-  BINOPGREATEREQ,  //!< BINOPGREATEREQ
-  BINOPCOMPNE,     //!< BINOPCOMPNE
-  BINOPCOMPEQ,     //!< BINOPCOMPEQ
-  BINOPADD,        //!< BINOPADD
-  BINOPELADD,      //!< BINOPELADD
-  BINOPSUB,        //!< BINOPSUB
-  BINOPELSUB,      //!< BINOPELSUB
-  BINOPDIV,        //!< BINOPDIV
-  BINOPELDIV,      //!< BINOPELDIV
-  BINOPMULT,       //!< BINOPMULT
-  BINOPELMULT,     //!< BINOPELMULT
-  BINOPEXP,        //!< BINOPEXP
-  BINOPELEXP,      //!< BINOPELEXP
-  BINOPnullptr     //!< BINOPnullptr
+  BINOPOR,
+  BINOPAND,
+  BINOPLOWER,
+  BINOPLOWEREQ,
+  BINOPGREATER,
+  BINOPGREATEREQ,
+  BINOPCOMPNE,
+  BINOPCOMPEQ,
+  BINOPADD,
+  BINOPELADD,
+  BINOPSUB,
+  BINOPELSUB,
+  BINOPDIV,
+  BINOPELDIV,
+  BINOPMULT,
+  BINOPELMULT,
+  BINOPEXP,
+  BINOPELEXP,
+  BINOPNULL
 };
 
-/**
- *
- */
-enum ElementType {
-  ELNONE,     //!< ELNONE
-  COMPONENT,  //!< COMPONENT
-  IMPORT,     //!< IMPORT
-  EXTENDS,    //!< EXTENDS
-  ELCLASS     //!< ELCLASS
-};
+enum ElementType { ELNONE, COMPONENT, IMPORT, EXTENDS, ELCLASS };
 
-/**
- *
- */
-enum EquationType {
-  EQNONE,      //!< EQNONE
-  EQEQUALITY,  //!< EQEQUALITY
-  EQCONNECT,   //!< EQCONNECT
-  EQCALL,      //!< EQCALL
-  EQFOR,       //!< EQFOR
-  EQWHEN,      //!< EQWHEN
-  EQIF         //!< EQIF
-};
+enum EquationType { EQNONE, EQEQUALITY, EQCONNECT, EQCALL, EQFOR, EQWHEN, EQIF };
 
-/**
- *
- */
 enum ExpressionType {
-  EXPNONE,           //!< EXPNONE
-  EXPCOMPREF,        //!< EXPCOMPREF
-  EXPBINOP,          //!< EXPBINOP
-  EXPDERIVATIVE,     //!< EXPDERIVATIVE
-  EXPNULL,           //!< EXPNULL
-  EXPEND,            //!< EXPEND
-  EXPIF,             //!< EXPIF
-  EXPCALLARG,        //!< EXPCALLARG
-  EXPBRACE,          //!< EXPBRACE
-  EXPCALL,           //!< EXPCALL
-  EXPELSEIF,         //!< EXPELSEIF
-  EXPCOLON,          //!< EXPCOLON
-  EXPUMINUS,         //!< EXPUMINUS
-  EXPBOOLEAN,        //!< EXPBOOLEAN
-  EXPSTRING,         //!< EXPSTRING
-  EXPREAL,           //!< EXPREAL
-  EXPINTEGER,        //!< EXPINTEGER
-  EXPBOOLEANNOT,     //!< EXPBOOLEANNOT
-  EXPOUTPUT,         //!< EXPOUTPUT
-  EXPRANGE,          //!< EXPRANGE
-  EXPNAMEDARGUMENT,  //!< EXPNAMEDARGUMENT
-  EXPARRAYINDEX      //!< EXPARRAYINDEX
+  EXPNONE,
+  EXPCOMPREF,
+  EXPBINOP,
+  EXPDERIVATIVE,
+  EXPNULL,
+  EXPEND,
+  EXPIF,
+  EXPCALLARG,
+  EXPBRACE,
+  EXPCALL,
+  EXPELSEIF,
+  EXPCOLON,
+  EXPUMINUS,
+  EXPBOOLEAN,
+  EXPSTRING,
+  EXPREAL,
+  EXPINTEGER,
+  EXPBOOLEANNOT,
+  EXPOUTPUT,
+  EXPRANGE,
+  EXPNAMEDARGUMENT,
+  EXPARRAYINDEX
 };
 
-/**
- *
- */
-enum ModificationType {
-  MODNONE,    //!< MODNONE
-  MODEQUAL,   //!< MODEQUAL
-  MODASSIGN,  //!< MODASSIGN
-  MODCLASS    //!< MODCLASS
-};
+enum ModificationType { MODNONE, MODEQUAL, MODASSIGN, MODCLASS };
 
-/**
- *
- */
-enum StatementType {
-  STNONE,      //!< STNONE
-  STRETURN,    //!< STRETURN
-  STBREAK,     //!< STBREAK
-  STWHEN,      //!< STWHEN
-  STASSING,    //!< STASSING
-  STFOR,       //!< STFOR
-  STIF,        //!< STIF
-  STWHILE,     //!< STWHILE
-  STOUTASSING  //!< STOUTASSING
-};
+enum StatementType { STNONE, STRETURN, STBREAK, STWHEN, STASSIGN, STFOR, STIF, STWHILE, STOUTASSING };
 
-/**
- *
- */
 enum TypePrefix {
-  TP_INPUT = (1 << 1),      //!< TP_INPUT
-  TP_OUTPUT = (1 << 2),     //!< TP_OUTPUT
-  TP_DISCRETE = (1 << 3),   //!< TP_DISCRETE
-  TP_PARAMETER = (1 << 4),  //!< TP_PARAMETER
-  TP_FLOW = (1 << 5),       //!< TP_FLOW
-  TP_STREAM = (1 << 6),     //!< TP_STREAM
-  TP_CONSTANT = (1 << 7),   //!< TP_CONSTANT
-  TP_FOR = (1 << 8),        //!< TP_FOR
-  TP_EQ = (1 << 9)          //!< TP_EQ
+  TP_INPUT = (1 << 1),
+  TP_OUTPUT = (1 << 2),
+  TP_DISCRETE = (1 << 3),
+  TP_PARAMETER = (1 << 4),
+  TP_FLOW = (1 << 5),
+  TP_STREAM = (1 << 6),
+  TP_CONSTANT = (1 << 7),
+  TP_FOR = (1 << 8),
+  TP_EQ = (1 << 9)
 };
 
-/**
- *
- */
-enum ArgumentType {
-  AR_MODIFICATION,  //!< AR_MODIFICATION
-  AR_REDECLARATION  //!< AR_REDECLARATION
-};
-/**
- *
- * @return
- */
+enum ArgumentType { AR_MODIFICATION, AR_REDECLARATION };
+
 AST_TypePrefix newAST_TypePrefix();
-/**
- *
- * @param
- * @return
- */
 AST_TypePrefix newAST_TypePrefix(AST_TypePrefix);
-/**
- *
- * @param
- * @param
- * @return
- */
 AST_TypePrefix AST_TypePrefixAdd(AST_TypePrefix, AST_TypePrefix);
-/**
- *
- */
+
 enum ClassPrefix {
-  CP_PARTIAL = (1 << 1),     //!< CP_PARTIAL
-  CP_CLASS = (1 << 2),       //!< CP_CLASS
-  CP_MODEL = (1 << 3),       //!< CP_MODEL
-  CP_BLOCK = (1 << 4),       //!< CP_BLOCK
-  CP_RECORD = (1 << 5),      //!< CP_RECORD
-  CP_CONNECTOR = (1 << 6),   //!< CP_CONNECTOR
-  CP_TYPE = (1 << 7),        //!< CP_TYPE
-  CP_OPERATOR = (1 << 8),    //!< CP_OPERATOR
-  CP_EXPANDABLE = (1 << 9),  //!< CP_EXPANDABLE
-  CP_PURE = (1 << 10),       //!< CP_PURE
-  CP_IMPURE = (1 << 11),     //!< CP_IMPURE
-  CP_PACKAGE = (1 << 12),    //!< CP_PACKAGE
-  CP_FUNCTION = (1 << 13)    //!< CP_FUNCTION
+  CP_PARTIAL = (1 << 1),
+  CP_CLASS = (1 << 2),
+  CP_MODEL = (1 << 3),
+  CP_BLOCK = (1 << 4),
+  CP_RECORD = (1 << 5),
+  CP_CONNECTOR = (1 << 6),
+  CP_TYPE = (1 << 7),
+  CP_OPERATOR = (1 << 8),
+  CP_EXPANDABLE = (1 << 9),
+  CP_PURE = (1 << 10),
+  CP_IMPURE = (1 << 11),
+  CP_PACKAGE = (1 << 12),
+  CP_FUNCTION = (1 << 13)
 };
 
-/**
- *
- * @param l
- * @param n
- * @return
- */
-#define current_element(it) (*it)
-/**
- *
- * @param l
- * @param n
- * @return
- */
-#define foreach(it, list) for (it = list->begin(); it != list->end(); it++)
-/**
- *
- * @param l
- * @param n
- * @return
- */
-#define foreachReverse(it, list) for (it = list->rbegin(); it != list->rend(); it++)
-
-/**
- *
- * @param l
- * @param n
- * @return
- */
 template <typename T1>
 T1 AST_ListElement(list<T1> *l, int n)
 {
@@ -613,12 +233,6 @@ T1 AST_ListElement(list<T1> *l, int n)
   return *it;
 }
 
-/**
- *
- * @param l
- * @param e
- * @return
- */
 template <typename T1>
 list<T1> *AST_ListAppend(list<T1> *l, T1 e)
 {
@@ -626,12 +240,6 @@ list<T1> *AST_ListAppend(list<T1> *l, T1 e)
   return l;
 }
 
-/**
- *
- * @param l
- * @param e
- * @return
- */
 template <typename T1>
 list<T1> *AST_ListPrepend(list<T1> *l, T1 e)
 {
@@ -639,12 +247,6 @@ list<T1> *AST_ListPrepend(list<T1> *l, T1 e)
   return l;
 }
 
-/**
- *
- * @param l
- * @param e
- * @return
- */
 template <typename T1>
 list<T1> *AST_ListRemove(list<T1> *l, T1 e)
 {
@@ -652,23 +254,12 @@ list<T1> *AST_ListRemove(list<T1> *l, T1 e)
   return l;
 }
 
-/**
- *
- * @param l
- * @return
- */
 template <typename T1>
 T1 AST_ListFirst(list<T1> *l)
 {
   return l->front();
 }
 
-/**
- *
- * @param l1
- * @param l2
- * @return
- */
 template <typename T1>
 list<T1> *AST_ListConcat(list<T1> *l1, list<T1> *l2)
 {
@@ -676,11 +267,6 @@ list<T1> *AST_ListConcat(list<T1> *l1, list<T1> *l2)
   return l1;
 }
 
-/**
- *
- * @param l1
- * @return
- */
 template <typename T1>
 list<T1> *AST_ListCopy(list<T1> *l1)
 {
@@ -689,23 +275,12 @@ list<T1> *AST_ListCopy(list<T1> *l1)
   return ls;
 }
 
-/**
- *
- * @param l1
- * @return
- */
 template <typename T1>
 int AST_Length(list<T1> *l1)
 {
   return l1->size();
 }
 
-/**
- *
- * @param l1
- * @param i
- * @return
- */
 template <typename T1>
 T1 AST_ListAt(list<T1> *l1, int i)
 {
@@ -719,11 +294,6 @@ T1 AST_ListAt(list<T1> *l1, int i)
   return T1();
 }
 
-/**
- *
- * @param t
- * @return
- */
 template <typename T1>
 list<T1> *newAST_SimpleList(T1 t)
 {
@@ -732,16 +302,6 @@ list<T1> *newAST_SimpleList(T1 t)
   return ls;
 }
 
-/**
- *
- * @param l1
- * @param ret
- * @param sec_name
- * @param separator
- * @param opener
- * @param closer
- * @param block
- */
 template <typename T1>
 void AST_ListPrint(list<T1> *l1, ostream &ret, string sec_name = "", string separator = " ", string opener = "", string closer = "",
                    bool block = false)
