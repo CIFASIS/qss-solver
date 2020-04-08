138606b1a4b0018fcded5c3150ccb01a32fc10c1
Joaquin <joaquin.f.fernandez@gmail.com>
2020-04-08 17:14:35 -0300

    Updated GT header files.

--- src/mmoc/tests/integration/gt_data/lotka_volterra/lotka_volterra.h
+++ src/mmoc/tests/integration/gt_data/lotka_volterra/lotka_volterra.h
@@ -1,5 +1,6 @@
 // Model data access macro.
-#define MODEL_DATA_ACCESS(m) double* x = m->x;
+#define MODEL_DATA_ACCESS(m) \
+  double* x = m->x; 
 
 // Coeff multipliers definition.
 #define COEFF_MULTIPLIER(c) COEFF_MULTIPLIER_##c
@@ -9,33 +10,36 @@
 #define COEFF_MULTIPLIER_3 6
 
 // Model Variables and Parameters Macros
-#define _idx_out_exp_1(d1) ((d1 - 1))
-#define _eval_out_exp_1(d1) ((d1 - 1))
+#define _idx_out_exp_1(d1) ((d1-1))
+#define _eval_out_exp_1(d1) ((d1-1))
 #define _is_var_out_exp_1(idx) idx >= 0 && idx < 2
-#define _get_out_exp_1_idxs(idx) _d1 = (idx - 0) + 1;
-#define _idx_x(d1, coeff) ((d1 - 1))
-#define _state_idx_x(d1, coeff) ((d1 - 1)) * 4 + coeff
-#define _x(d1, coeff) x[_state_idx_x(d1, coeff)] * COEFF_MULTIPLIER(coeff)
-#define _init_x(d1, coeff) x[_state_idx_x(d1, coeff)]
-#define _eval_x(d1, coeff) ((d1 - 1))
+#define _get_out_exp_1_idxs(idx) \
+		_d1 = (idx-0)+ 1;
+#define _idx_x(d1,coeff) ((d1-1))
+#define _state_idx_x(d1,coeff) ((d1-1))*4 + coeff
+#define _x(d1,coeff) x[_state_idx_x(d1,coeff)] * COEFF_MULTIPLIER(coeff)
+#define _init_x(d1,coeff) x[_state_idx_x(d1,coeff)]
+#define _eval_x(d1,coeff) ((d1-1))
 #define _is_var_x(idx) idx >= 0 && idx < 2
-#define _get_x_idxs(idx) _d1 = (idx - 0) + 1;
-#define _eval_dep_x(d1, coeff) dx[_state_idx_x(d1, coeff)]
+#define _get_x_idxs(idx) \
+		_d1 = (idx-0)+ 1;
+#define _eval_dep_x(d1,coeff) dx[_state_idx_x(d1,coeff)]
 
 // Derivative Equations Macros
 
 // Event Macros
 
 // Output Equations Macros
-#define _apply_usage_out_exp_1(_d1) _d1 = _d1;
+#define _apply_usage_out_exp_1(_d1) \
+	_d1 = _d1;
 #define _out out[0]
 
 // Input Matrix Macros
 
-// Jacobian Macros definition.
+// Jacobian Macros definition. 
 #define _jac(i) jac[i++]
 
 #define _time t
 
-// Derivative Macros definition.
-#define _der_x(d1, coeff) dx[coeff + 1]
+// Derivative Macros definition. 
+#define _der_x(d1,coeff) dx[coeff+1]
