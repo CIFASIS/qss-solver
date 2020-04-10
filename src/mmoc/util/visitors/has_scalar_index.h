fcbee8bab991df1b93d1c5aada91a6e2f9ea9291
Joaquin <joaquin.f.fernandez@gmail.com>
2020-04-10 19:27:58 -0300

    Added new HasScalarIndex method.

---
+++ src/mmoc/util/visitors/has_scalar_index.h
@@ -0,0 +1,43 @@
+/*****************************************************************************
+
+ This file is part of QSS Solver.
+
+ QSS Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QSS Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#ifndef HAS_SCALAR_INDEX_H_
+#define HAS_SCALAR_INDEX_H_
+
+#include "../../ast/ast_builder.h"
+#include "../../deps/graph/graph_helpers.h"
+#include "../../ir/index.h"
+#include "../ast_util.h"
+
+namespace MicroModelica {
+namespace Util {
+class HasScalarIndex : public AST_Expression_Visitor<bool> {
+  public:
+  HasScalarIndex();
+  ~HasScalarIndex() = default;
+
+  private:
+  bool foldTraverseElement(AST_Expression exp);
+  inline bool foldTraverseElementUMinus(AST_Expression exp) { return apply(exp->getAsUMinus()->exp()); }
+  inline bool foldTraverseElement(bool l, bool r, BinOpType bot) { return l && r; }
+};
+
+}  // namespace Util
+}  // namespace MicroModelica
+#endif /* HAS_SCALAR_INDEX_H_ */
