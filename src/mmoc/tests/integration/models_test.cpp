5e687af2e98c3892066dc6fadde1852ccae989f8
Joaquin <joaquin.f.fernandez@gmail.com>
2020-04-10 19:22:27 -0300

    Added new integation tests.

--- src/mmoc/tests/integration/models_test.cpp
+++ src/mmoc/tests/integration/models_test.cpp
@@ -49,8 +49,8 @@ TEST_P(ITests, GenerateCode)
   std::ifstream result(RESULT_FILE.c_str());
   EXPECT_TRUE(result.good());
 }
-
-const char* models[] = {"advection", "lc_line", "lotka_volterra"};
+ 
+const char* models[] = {"advection", "lc_line", "lotka_volterra", "bball_downstairs", "airconds", "aircont", "boost", "buck", "buckboost", "cuk", "cuk2", "buck_circuit", "inverters", "rectifier", "spikings"};
 
 INSTANTIATE_TEST_SUITE_P(Models, ITests, testing::ValuesIn(models));
 
