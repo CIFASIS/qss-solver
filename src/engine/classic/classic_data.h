2b41851751a82f3877f2e2a372c98341308aef60
Joaquin <joaquin.f.fernandez@gmail.com>
2020-06-24 15:19:01 -0300

    Removed empty doc.

--- src/engine/classic/classic_data.h
+++ src/engine/classic/classic_data.h
@@ -23,172 +23,88 @@
 #include "../common/data.h"
 #include "../common/utils.h"
 
-/**
- *
- */
 typedef void (*CLC_fp)(void);
-/**
- *
- * @param
- * @return
- */
+
 typedef int (*CLC_fpr)(int);
-/**
- *
- * @param
- */
+
 typedef void (*CLC_fpa)(int);
-/**
- *
- * @param
- * @param
- * @param
- * @param
- */
+
 typedef void (*CLC_eq)(double *, double *, double *, double, double *);
-/**
- *
- * @param
- * @param
- * @param
- * @param
- * @param
- */
+
 typedef void (*CLC_zc)(int, double *, double *, double *, double, double *);
-/**
- *
- * @param
- * @param
- * @param
- * @param
- */
+
 typedef void (*CLC_hnd)(int, double *, double *, double *, double);
-/**
- *
- * @param
- * @param
- */
+
 typedef void (*CLC_input)(double, double *);
-/**
- *
- */
+
 typedef struct CLC_event_ *CLC_event;
-/**
- *
- */
+
 struct CLC_event_ {
-  CLC_zc zeroCrossing;  //!<
-  CLC_hnd handlerPos;   //!<
-  CLC_hnd handlerNeg;   //!<
+  CLC_zc zeroCrossing;
+  CLC_hnd handlerPos;
+  CLC_hnd handlerNeg;
 };
 
 typedef void (*CLC_jac)(double *x, double *d, double *alg, double t, double *j);
-/**
- *
- * @param zeroCrossing
- * @param handlerPos
- * @param handlerNeg
- * @return
- */
+
 CLC_event CLC_Event(CLC_zc zeroCrossing, CLC_hnd handlerPos, CLC_hnd handlerNeg);
-/**
- *
- * @param events
- */
+
 void CLC_freeEvent(CLC_event events);
-/**
- *
- */
+
 typedef struct CLC_data_ *CLC_data;
 
-/**
- *
- */
 struct CLC_data_ {
-  double *dQMin;  //!<
-  double *dQRel;  //!<
-  double *d;      //!<
-  double *x;      //!<
-  double *alg;    //!<
-  double it;      //!<
-  double ft;      //!<
-  int *nSD;       //!<
-  int *nDS;       //!<
-  int **SD;       //!<
-  int **DS;       //!<
-  int *IT;        //!<
-  int states;     //!<
-  int discretes;  //!<
-  int events;     //!<
-  int inputs;     //!<
+  double *dQMin;
+  double *dQRel;
+  double *d;
+  double *x;
+  double *alg;
+  double it;
+  double ft;
+  int *nSD;
+  int *nDS;
+  int **SD;
+  int **DS;
+  int *IT;
+  int states;
+  int discretes;
+  int events;
+  int inputs;
   char *fired;
-  unsigned long scalarEvaluations;         //!<
-  unsigned long zeroCrossings;             //!<
-  unsigned long long funEvaluations;       //!<
-  unsigned long totalOutputSteps;          //!<
-  unsigned long totalJacobians;            //!<
-  unsigned long totalCrossingEvaluations;  //!<
-  unsigned long totalStepsDASSL;           //!<
-  unsigned long totalSteps;                //!<
-  unsigned long totalEvents;               //!<
-  SD_Solver solver;                        //!<
-  SD_eventData event;                      //!<
-  SD_parameters params;                    //!<
+  unsigned long scalarEvaluations;
+  unsigned long zeroCrossings;
+  unsigned long long funEvaluations;
+  unsigned long totalOutputSteps;
+  unsigned long totalJacobians;
+  unsigned long totalCrossingEvaluations;
+  unsigned long totalStepsDASSL;
+  unsigned long totalSteps;
+  unsigned long totalEvents;
+  SD_Solver solver;
+  SD_eventData event;
+  SD_parameters params;
 };
-/**
- *
- * @param states
- * @param discretes
- * @param events
- * @param inputs
- * @param name
- * @return
- */
+
 CLC_data CLC_Data(int states, int discretes, int events, int inputs, int algebraics, string name);
-/**
- *
- * @param data
- */
+
 void CLC_freeData(CLC_data data);
-/**
- *
- * @param data
- */
+
 void CLC_allocDataMatrix(CLC_data data);
-/**
- *
- */
+
 typedef struct CLC_model_ *CLC_model;
-/**
- *
- */
+
 struct CLC_model_ {
   CLC_eq f; /**< Model definition \f$ \forall \imath \in [0,DIM] f(x_{i}(t),t)_{i} \f$ 		*/
   CLC_jac jac;
-  CLC_event events;  //!<
+  CLC_event events;
 };
-/**
- *
- * @param f
- * @param zeroCrossing
- * @param handlerPos
- * @param handlerNeg
- * @return
- */
+
 CLC_model CLC_Model(CLC_eq f, CLC_zc zeroCrossing, CLC_hnd handlerPos, CLC_hnd handlerNeg, CLC_jac);
-/**
- *
- * @param model
- */
+
 void CLC_freeModel(CLC_model model);
-/**
- *
- * @param
- * @param SD_output
- * @param
- * @param SD_simulationSettings
- */
+
 typedef void (*CLC_setData)(CLC_data, SD_output, CLC_model, SD_simulationSettings);
 
 void CLC_allocDataMatrix(CLC_data data);
+
 #endif /* CLASSIC_DATA_H_ */
--- src/engine/common/simulator.h
+++ src/engine/common/simulator.h
@@ -22,98 +22,43 @@
 
 #include "data.h"
 
-/**
- *
- */
 typedef struct SIM_simulator_ *SIM_simulator;
 
-/**
- *
- */
 typedef struct SIM_simulatorState_ *SIM_simulatorState;
 
-/**
- *
- */
 typedef struct SIM_simulatorOps_ *SIM_simulatorOps;
 
-/**
- *
- * @param
- */
 typedef void (*SIM_simulateFn)(SIM_simulator);
 
-/**
- *
- * @param
- */
 typedef void (*SIM_freeSimulatorFn)(SIM_simulator);
 
-/**
- *
- */
 struct SIM_simulatorOps_ {
   SIM_simulateFn simulate;            //!<
   SIM_freeSimulatorFn freeSimulator;  //!<
 };
 
-/**
- *
- */
 struct SIM_simulatorState_ {
   void *sim;                       //!<
   SD_simulationSettings settings;  //!<
 };
 
-/**
- *
- */
 struct SIM_simulator_ {
   SIM_simulatorState state;  //!<
   SIM_simulatorOps ops;      //!<
 };
 
-/**
- *
- * @param settings
- * @return
- */
 SIM_simulator SIM_Simulator(SD_simulationSettings settings);
 
-/**
- *
- * @param simulator
- */
 void SIM_freeSimulator(SIM_simulator simulator);
 
-/**
- *
- * @return
- */
 SIM_simulatorOps SIM_SimulatorOps();
 
-/**
- *
- * @param ops
- */
 void SIM_freeSimulatorOps(SIM_simulatorOps ops);
 
-/**
- *
- * @return
- */
 SIM_simulatorState SIM_SimulatorState();
 
-/**
- *
- * @param state
- */
 void SIM_freeSimulatorState(SIM_simulatorState state);
 
-/**
- *
- * @param simulator
- */
 void SIM_simulate(SIM_simulator simulator);
 
 #endif /* SIMULATOR_H_ */
