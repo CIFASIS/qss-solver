3435689e0e20e32b0b3766c4949a9fcc7b5dc1e1
Joaquin <joaquin.f.fernandez@gmail.com>
2020-06-24 14:19:43 -0300

    Added Jacobian generator.

---
+++ src/mmoc/ir/jacobian.cpp
@@ -0,0 +1,188 @@
+/*****************************************************************************
+
+ This file is part of QSS Solver.
+
+ QSS Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QSS Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#include "jacobian.h"
+
+#include <sstream>
+
+#include "../deps/builders/sd_sb_graph_builder.h"
+#include "../deps/sb_dependencies.h"
+#include "../parser/parse.h"
+#include "../util/util.h"
+#include "../util/symbol_table.h"
+#include "derivative.h"
+#include "helpers.h"
+#include "index.h"
+
+namespace MicroModelica {
+using namespace Deps;
+using namespace Deps::SBG;
+using namespace Util;
+namespace IR {
+
+JacGenerator::JacGenerator() : _jac_def(), _tabs(0) {}
+
+void JacGenerator::postProcess(SBG::VertexProperty vertex)
+{
+  int size = vertex.size();
+  int id = vertex.id();
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  code << tab << "r = 0;" << endl;
+  code << tab << "for (col = 0; col < dvdx.dfdx[" << id << "].size[r]; col++) {" << endl;
+  code << tab << "  for (row = r; row < r + " << size << "; row++) {" << endl;
+  code << tab << "    jac(jit) = dvdx.dfdx[].value[row-r][col];" << endl;
+  code << tab << "  }" << endl;
+  code << tab << "}" << endl;
+  code << tab << "r += " << size << endl;
+  _jac_def.code.append(code.str());
+}
+
+void JacGenerator::init(SBG::VertexProperty vertex)
+{
+  stringstream code;
+  code << "for(row = 1; row <= " << vertex.size() << "; row++) {" << endl;
+  _tabs++;
+  _jac_def.code.append(code.str());
+}
+
+void JacGenerator::end()
+{
+  _jac_def.code.append("}\n");
+  _tabs--;
+}
+
+void JacGenerator::dependencyPrologue(Equation eq, SBG::VariableDep var_dep, SBG::Map map)
+{
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  Range range(var_dep.mdi());
+  vector<string> variables;
+  variables.push_back("row");
+  vector<string> exps = map.exps(variables);
+  code << tab << range.in(exps);
+  Expression i_exp = Expression::generate(var_dep.var().name(), exps);
+  Index x_ind(i_exp);
+  code << tab << tab << "x_ind = " << x_ind << ";" << endl;
+  _jac_def.code.append(code.str());
+  _tabs++;
+}
+
+void JacGenerator::dependencyEpilogue()
+{
+  _tabs--;
+  string end = Utils::instance().tabs(_tabs) + "}\n";
+  _jac_def.code.append(end);
+}
+
+void JacGenerator::generatePos(int id, EQUATION::Type type, string row, string col)
+{
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  code << tab << col << " = pos(dvdx->";
+  if (type == EQUATION::Algebraic) {
+    code << "dgdx";
+  } else {
+    code << "dfdx";
+  }
+  code << "[" << id << "].index[" << row << "], x_ind);" << endl;
+  _jac_def.code.append(code.str());
+}
+
+void JacGenerator::generateEquation(int id, EQUATION::Type type)
+{
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  string mat = (type == EQUATION::Algebraic) ? "dgdx" : "dfdx";
+  code << tab << "dvdx->" << mat << "[" << id << "].value[row][col] +=  aux;" << endl;
+  _jac_def.code.append(code.str());
+}
+
+void JacGenerator::generateEquation(int v_id, int g_id, EQUATION::Type type)
+{
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  string mat = (type == EQUATION::Algebraic) ? "dgdx" : "dfdx";
+  code << tab << "dvdx->" << mat << "dfdx[" << v_id << "].value[row][col] += ";
+  code << "aux * dvdx->dgdx[" << g_id << "].value[row_g][col_g];" << endl;
+  _jac_def.code.append(code.str());
+}
+
+void JacGenerator::visitF(Equation eq, SBG::VariableDep var_dep, SBG::Map map)
+{
+  stringstream code;
+  VarSymbolTable symbols = Utils::instance().symbols();
+  dependencyPrologue(eq, var_dep, map);
+  string tab = Utils::instance().tabs(_tabs);
+  generatePos(eq.id(), eq.type());
+  code << tab << "aux = " << ExpressionDerivator::partialDerivative(eq, Index(map.exp())) << ";" << endl;
+  _jac_def.code.append(code.str());
+  generateEquation(eq.id(), eq.type());
+  dependencyEpilogue();
+}
+
+void JacGenerator::visitG(Equation v_eq, Equation g_eq, SBG::VariableDep var_dep, SBG::Map n_map, Map map_m, SBG::Offset index_shift)
+{
+  stringstream code;
+  dependencyPrologue(g_eq, var_dep, n_map);
+  generatePos(v_eq.id(), v_eq.type());
+  vector<string> variables;
+  variables.push_back("row");
+  vector<string> exps = map_m.exps(variables);
+  int shift = index_shift[0];
+  string tab = Utils::instance().tabs(_tabs);
+  code << tab << "row_g = ";
+  for (string map : exps) {
+    code << map << " - " << shift;
+  }
+  code << ";" << endl;
+  _jac_def.code.append(code.str());
+  generatePos(g_eq.id(), g_eq.type(), "row_g", "col_g");
+  generateEquation(v_eq.id(), g_eq.id(), v_eq.type());
+  dependencyEpilogue();
+}
+
+void JacGenerator::initG(Equation eq, SBG::Map map_m)
+{
+  stringstream code;
+  string tab = Utils::instance().tabs(_tabs);
+  code << tab << "aux = " << ExpressionDerivator::partialDerivative(eq, Index(map_m.exp())) << endl;
+  _jac_def.code.append(code.str());
+}
+
+JacDef JacGenerator::deps() { return _jac_def; }
+
+Jacobian::Jacobian() {}
+
+void Jacobian::build()
+{
+  EquationTable algebraics = ModelConfig::instance().algebraics();
+  EquationTable derivatives = ModelConfig::instance().derivatives();
+  VarSymbolTable symbols = Utils::instance().symbols();
+  JacobianBuilder jac;
+  IndexShiftBuilder index_shifts(algebraics, symbols);
+  SDSBGraphBuilder SDSBGraph = SDSBGraphBuilder(derivatives, algebraics, symbols);
+  jac.compute(SDSBGraph.build(), index_shifts.build());
+  _jac_def = jac.deps();
+}
+
+string Jacobian::code() { return _jac_def.code; }
+
+}  // namespace IR
+}  // namespace MicroModelica
---
+++ src/mmoc/ir/jacobian.h
@@ -0,0 +1,83 @@
+/*****************************************************************************
+
+ This file is part of QSS Solver.
+
+ QSS Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QSS Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QSS Solver.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#ifndef JACOBIAN_H_
+#define JACOBIAN_H_
+
+#include <string>
+#include <map>
+
+#include "equation.h"
+#include "../deps/graph/sb_graph.h"
+#include "../deps/graph/sb_graph_helpers.h"
+#include "../util/symbol_table.h"
+#include "../util/table.h"
+
+namespace MicroModelica {
+namespace IR {
+
+struct JacDef {
+  std::string code;
+};
+
+class JacGenerator {
+  public:
+  JacGenerator();
+  ~JacGenerator() = default;
+
+  void init(Deps::SBG::VertexProperty vertex);
+  void end();
+  void postProcess(Deps::SBG::VertexProperty vertex);
+  void visitF(Equation eq, Deps::SBG::VariableDep var_dep, Deps::SBG::Map map);
+  void visitG(Equation v_eq, Equation g_eq, Deps::SBG::VariableDep var_dep, Deps::SBG::Map n_map, Deps::SBG::Map map_m,
+              Deps::SBG::Offset index_shift);
+  void initG(Equation eq, Deps::SBG::Map map_m);
+  JacDef deps();
+
+  protected:
+  IR::Expression generateExp(string var_name, vector<string> indices);
+  void dependencyPrologue(Equation eq, Deps::SBG::VariableDep var_dep, Deps::SBG::Map map, Deps::SBG::Offset index_shift);
+  void dependencyPrologue(Equation eq, Deps::SBG::VariableDep var_dep, Deps::SBG::Map map);
+  void dependencyEpilogue();
+  void updateMatrix(std::map<std::string, std::set<std::string>>& matrix);
+  void generatePos(int id, EQUATION::Type type, std::string row = "row", std::string col = "col");
+  void generateEquation(int id, EQUATION::Type type);
+  void generateEquation(int v_id, int g_id, EQUATION::Type type);
+
+  JacDef _jac_def;
+  int _tabs;
+};
+
+class Jacobian {
+  public:
+  Jacobian();
+  ~Jacobian() = default;
+
+  void build();
+
+  std::string code();
+
+  protected:
+  JacDef _jac_def;
+};
+
+}  // namespace IR
+}  // namespace MicroModelica
+
+#endif /* JACOBIAN_H_ */
