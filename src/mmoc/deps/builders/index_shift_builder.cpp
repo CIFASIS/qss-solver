7924d5a045e38a32154bd4c5f72816cdfab2e5b8
Joaquin <joaquin.f.fernandez@gmail.com>
2020-10-20 17:17:58 -0300

    Fix index shift offset builder.

--- src/mmoc/deps/builders/index_shift_builder.cpp
+++ src/mmoc/deps/builders/index_shift_builder.cpp
@@ -21,6 +21,7 @@
 
 #include "../graph/sb_graph.h"
 #include "../../util/util_types.h"
+#include "../../util/visitors/eval_init_exp.h"
 
 namespace MicroModelica {
 using namespace IR;
@@ -37,10 +38,35 @@ IndexShift IndexShiftBuilder::build()
 {
   EquationTable::iterator eq_it;
   for (Equation eq = _algebraics.begin(eq_it); !_algebraics.end(eq_it); eq = _algebraics.next(eq_it)) {
-    EvalOccur eval_occur(eq.lhs(), _symbols, eq.range());
-    if (eval_occur.hasIndex()) {
-      _index_shift[eq.id()] = eval_occur.offsets();
+    Expression lhs = eq.lhs();
+    SBG::Offset index_shift;
+    vector<int> vals;
+    Option<Variable> var = eq.LHSVariable();
+    if (eq.hasRange()) {
+      EvalOccur eval_occur(eq.lhs(), _symbols, eq.range());
+      SBG::LinearFunction lf = eval_occur.linearFunctions();
+      RangeDefinitionTable ranges = eq.range()->definition();
+      RangeDefinitionTable::iterator it;
+      for (RangeDefinition range = ranges.begin(it); !ranges.end(it); range = ranges.next(it)) {
+        // Use cBegin here to get minus 1 that we need for the shift.
+        vals.push_back(range.cBegin());
+      }
+      int val = Variable::eval(var.get(), lf.apply(vals)) - 1;
+      index_shift.add(val);
+    } else {
+      if (lhs.isScalar()) {
+        EvalInitExp eval_exp(_symbols);
+        ExpressionList indexes = lhs.indexes();
+        for (Expression index : indexes) {
+          vals.push_back(eval_exp.apply(index.expression()));
+        }
+        int val = Variable::eval(var.get(), vals) - 1;
+        index_shift.add(val);
+      } else {
+        index_shift.add(0);
+      }
     }
+    _index_shift[eq.id()] = index_shift;
   }
   return _index_shift;
 }
