37e79724ea298b921bede00391efbff60108bfd7
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-06-06 17:19:45 -0300

    Parse all for indexes in statements.

--- src/mmoc/ir/mmo_ir.h
+++ src/mmoc/ir/mmo_ir.h
@@ -17,8 +17,7 @@
 
  ******************************************************************************/
 
-#ifndef MMO_MICROMODELICA_IR_H_
-#define MMO_MICROMODELICA_IR_H_
+#pragma once
 
 #include <list>
 #include <string>
@@ -30,152 +29,40 @@
 namespace MicroModelica {
 namespace IR {
 
-/**
- *
- */
 class MicroModelicaIR : public AST_Visitor {
   public:
-  /**
-   *
-   * @param name
-   */
   MicroModelicaIR(string name);
-  /**
-   *
-   */
   ~MicroModelicaIR();
-  /**
-   *
-   * @return
-   */
   StoredDefinition storedDefinition();
-  /**
-   *
-   * @return
-   */
   list<Class> classes() const;
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Class x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_Class x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Composition x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_Composition x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_CompositionElement x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_CompositionElement x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_CompositionEqsAlgs x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_CompositionEqsAlgs x);
-  /**
-   *
-   * @param
-   */
   void visit(AST_External_Function_Call);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Element x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Modification x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_Modification x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Comment x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Equation x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_ForIndex x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Equation_Else x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Expression x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Argument x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Statement x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_Statement x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_Statement_Else x);
-  /**
-   *
-   * @param x
-   */
   void visit(AST_StoredDefinition x);
-  /**
-   *
-   * @param x
-   */
   void leave(AST_StoredDefinition x);
-  /**
-   *
-   * @param x
-   * @return
-   */
   int apply(AST_Node x);
   inline const StoredDefinition definition() { return _std; };
+
+  protected:
+  void visitForStms(AST_Statement_For for_stm);
 
   private:
   StoredDefinition _std;
@@ -187,4 +74,3 @@ class MicroModelicaIR : public AST_Visitor {
 };
 }  // namespace IR
 }  // namespace MicroModelica
-#endif /* MICROMODELICA_IR_H_ */
