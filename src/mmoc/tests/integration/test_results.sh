67bf7d3d0b07f0ef8346908f5549ef70b2e40105
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-06-24 17:52:26 -0300

    Show test file results on failure.

--- src/mmoc/tests/integration/test_results.sh
+++ src/mmoc/tests/integration/test_results.sh
@@ -11,6 +11,7 @@ test_results ()
     if [ -n "$RESULT" ]; then
         echo "Model " $MODEL " test failed for " $TEST_FILE
         echo $RESULT
+        echo $TEST_FILE
         diff "$GT_FILE" "$TEST_FILE" > $TEST_FILE.failed
     fi     
 }
