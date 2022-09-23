6e62dc719f4064fcfef5bf62142b12af78bc758c
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-09-23 11:38:58 -0300

    Updated build script.

--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -15,26 +15,26 @@
 #         NOTES: --- 
 #        AUTHOR: Joaquin Fernandez, joaquin.f.fernandez@gmail.com
 #       PROJECT: QSS Solver
-#       VERSION: 3.2
+#       VERSION: 4.0
 #===================================================================================
 
-rm -rf qss-solver-amd64.deb
+rm -rf qss-solver-*.deb
 
 cd ../../
 ARCH=`uname -m`
 echo "Retrieving latest from Git";
-./deploy/common/repos.sh pull
-VM=`head ./doc/version.major -c 4`
-VMC=`head ./doc/version.major -c 3`
-
-./deploy/common/repos.sh tag ${VM} 
-
-REV=`./deploy/common/repos.sh version`
+git pull
 
 cat ./deploy/linux/qss-solver.ini.in > ./deploy/linux/qss-solver.ini
-./deploy/common/setRevision.sh ./deploy/linux/qss-solver.ini $REV $VMC
+# Set solver version
+VER=`cat ./deploy/linux/version`
+INI_VER="version="$VER
+echo $INI_VER >> ./deploy/linux/qss-solver.ini
 
-VER=$VM$REV
+# Set solver branch
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+BRANCH="branch="$BRANCH 
+echo $BRANCH >> ./deploy/linux/qss-solver.ini
 
 echo "Building QSS Solver DEB package for $ARCH version $VER";
 echo "Building Binaries";
@@ -46,7 +46,10 @@ rm -rf tmp_deb
 rm -rf tmp
 mkdir tmp_deb
 mkdir tmp
-./deploy/common/repos.sh deploy tmp
+CHECKOUT_PATH=$MMOC_PATH/tmp/
+mkdir -p $CHECKOUT_PATH
+git checkout-index -a -f --prefix=$CHECKOUT_PATH 
+
 cp -r ./tmp/deploy/linux/deb/* ./tmp_deb/
 chmod 0755 tmp_deb/DEBIAN/post*
 mkdir ./tmp_deb/opt
@@ -56,14 +59,14 @@ mkdir ./tmp_deb/opt/qss-solver/src
 mkdir ./tmp_deb/opt/qss-solver/build
 mkdir ./tmp_deb/opt/qss-solver/output
 mkdir ./tmp_deb/opt/qss-solver/lib/
-cp ./tmp/deploy/linux/scripts/* ./tmp_deb/opt/qss-solver/bin/
 
 cat ./tmp_deb/DEBIAN/control.amd64 | awk -v VERSION="$VER" '{ if(index($0,"Version: ")>=1) print "Version: " VERSION ; else print $0;}' >  ./tmp_deb/DEBIAN/control
 rm ./tmp_deb/DEBIAN/control.amd64; 
 
-cp bin/mmoc  ./tmp_deb/opt/qss-solver/bin/ 
-cp bin/qss-solver ./tmp_deb/opt/qss-solver/bin/ 
-cp bin/translate-sbml  ./tmp_deb/opt/qss-solver/bin/
+cp deploy/linux/version ./tmp_deb/opt/qss-solver/
+cp src/mmoc/usr/bin/mmoc  ./tmp_deb/opt/qss-solver/bin/ 
+cp src/gui/usr/bin/qss-solver ./tmp_deb/opt/qss-solver/bin/ 
+cp src/interfaces/sbml/usr/bin/translate-sbml  ./tmp_deb/opt/qss-solver/bin/
 cp src/engine/3rd-party/partitioners/hmetis/khmetis ./tmp_deb/opt/qss-solver/bin/
 cp deploy/linux/qss-solver.ini ./tmp_deb/opt/qss-solver/bin/qss-solver.ini
 cp deploy/images/integrator.svg ./tmp_deb/opt/qss-solver/bin/
@@ -72,6 +75,7 @@ cp LICENSE ./tmp_deb/opt/qss-solver/
 cp INSTALL ./tmp_deb/opt/qss-solver/
 cp README.md ./tmp_deb/opt/qss-solver/
 cp CHANGELOG ./tmp_deb/opt/qss-solver/
+cp ./tmp/bin/*.sh ./tmp_deb/opt/qss-solver/bin/
 cp -r ./tmp/doc ./tmp_deb/opt/qss-solver/
 cp -r ./tmp/models ./tmp_deb/opt/qss-solver/
 cp -r ./tmp/packages ./tmp_deb/opt/qss-solver/
@@ -95,11 +99,9 @@ rm -rf ./tmp_deb/opt/qss-solver/src/usr/lib
 rm -rf ./tmp_deb/opt/qss-solver/src/interfaces/sbml/usr
 rm -rf ./tmp_deb/opt/qss-solver/src/gui/usr
 
-cp /usr/lib/libsbml.so.5.12.0 ./tmp_deb/opt/qss-solver/lib/libsbml.so.5
+cp /usr/lib/x86_64-linux-gnu/libsbml.so.5.18.0 ./tmp_deb/opt/qss-solver/lib/libsbml.so.5
 cp src/engine/3rd-party/partitioners/patoh/Linux-x86_64/libpatoh.a ./tmp_deb/opt/qss-solver/lib/libpatoh.a
 cp src/engine/3rd-party/partitioners/metis/Linux-x86_64/libmetis.a ./tmp_deb/opt/qss-solver/lib/libmetis.a
-cp src/engine/3rd-party/partitioners/scotch/Linux-x86_64/libscotch.a ./tmp_deb/opt/qss-solver/lib/libscotch.a
-cp src/engine/3rd-party/partitioners/scotch/Linux-x86_64/libscotcherr.a ./tmp_deb/opt/qss-solver/lib/libscotcherr.a
 
 chmod 0644 `find tmp_deb/ -iname *.cpp`
 chmod 0644 `find tmp_deb/ -iname *.c`
@@ -113,7 +115,7 @@ chmod 0644 `find tmp_deb/opt/qss-solver/packages/ -type f`
 chmod 0644 `find tmp_deb/opt/qss-solver/src/usr/ -type f`
 chmod 0755 `find tmp_deb/ -type d`
 fakeroot dpkg -b tmp_deb qss-solver.deb
-mv qss-solver.deb ./deploy/linux/qss-solver-amd64.deb
+mv qss-solver.deb ./deploy/linux/qss-solver-$VER.deb
 rm -rf tmp_deb
 rm -rf tmp
 cd deploy/linux
