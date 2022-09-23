9a164415fcbd1bf7cca912ce0a7fec354968d358
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-09-23 12:32:40 -0300

    [iss-180] Squashed commit of the following:
    commit 9186b746c76776ebdda2c1abf16ef128e890007d
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 12:14:38 2022 -0300
    
        Cleanup dassl integrator code.

--- src/engine/classic/classic_dassl_integrator.h
+++ src/engine/classic/classic_dassl_integrator.h
@@ -17,115 +17,28 @@
 
  ******************************************************************************/
 
-#ifndef DASSL_INTEGRATOR_H_
-#define DASSL_INTEGRATOR_H_
+#pragma once
 
-#include "../common/simulator.h"
+#include <common/simulator.h>
 
-/**
- *
- * @param t
- * @param x
- * @param dx
- * @param CJ
- * @param res
- * @param iRes
- * @param rPar
- * @param iPar
- */
 typedef void (*dae_system)(const double *t, const double *x, const double *dx, const double *CJ, double *res, int iRes, const double *rPar,
                            const int *iPar);
-/**
- *
- * @param n
- * @param t
- * @param x
- * @param dx
- * @param nrt
- * @param res
- * @param rPar
- * @param iPar
- */
+
 typedef void (*zero_crossing)(const int *n, const double *t, const double *x, const double *dx, const int *nrt, double *res,
                               const double *rPar, const int *iPar);
-/**
- *
- */
+
 typedef void (*nullfunc)();
 
-/**
- *
- * @param s
- * @param noOfEquations
- * @param currentTime
- * @param x
- * @param dx
- * @param finalTime
- * @param info
- * @param relativeTolerance
- * @param absoluteTolerance
- * @param outputStatusFlag
- * @param dWorkArray
- * @param lengthOfDWork
- * @param iWorkArray
- * @param lengthOfIWork
- * @param rParArray
- * @param iParArray
- * @param jac
- * @param p
- * @param r
- * @param numConstrains
- * @param numOfOutputs
- */
-void ddaskr_(dae_system s,               // 1
-             const int *noOfEquations,   // 2
-             double *currentTime,        // 3
-             double *x,                  // 4
-             double *dx,                 // 5
-             const double *finalTime,    // 6
-             int info[20],               // 7
-             double *relativeTolerance,  // 8
-             double *absoluteTolerance,  // 9
-             int *outputStatusFlag,      // 10
-             double *dWorkArray,         // 11
-             const int *lengthOfDWork,   // 12
-             int *iWorkArray,            // 13
-             const int *lengthOfIWork,   // 14
-             const double *rParArray,    // 15
-             const int *iParArray,       // 16
-             nullfunc jac,               // 17
-             nullfunc p,                 // 18
-             zero_crossing r,            // 19
-             const int *numConstrains,   // 20
-             int *numOfOutputs           // 21
-);
-/**
- *
- * @param n
- * @param t
- * @param x
- * @param dx
- * @param nrt
- * @param res
- * @param rPar
- * @param iPar
- */
+void ddaskr_(dae_system s, const int *noOfEquations, double *currentTime, double *x, double *dx, const double *finalTime, int info[20],
+             double *relativeTolerance, double *absoluteTolerance, int *outputStatusFlag, double *dWorkArray, const int *lengthOfDWork,
+             int *iWorkArray, const int *lengthOfIWork, const double *rParArray, const int *iParArray, nullfunc jac, nullfunc p,
+             zero_crossing r, const int *numConstrains, int *numOfOutputs);
+
 void DASSL_events(const int *n, const double *t, const double *x, const double *dx, const int *nrt, double *res, const double *rPar,
                   const int *iPar);
 
-/**
- *
- * @param x
- */
 void DASSL_reset_x(double *x);
-/**
- *
- * @param x
- */
-void DASSL_update_x(const double *x);
-/**
- *
- */
-void DASSL_integrate(SIM_simulator simulator);
 
-#endif /* DASSL_INTEGRATOR_H_ */
+void DASSL_update_x(const double *x);
+
+void DASSL_integrate(SIM_simulator simulator);
