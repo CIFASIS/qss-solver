734fc2fe882268fcc63d9f1e47ebaccdc5f16439
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-09-27 10:33:25 -0300

    [iss-182] Squashed commit of the following:
    commit ba7e6a20a841712c1b8bf5ea945a058958e5800b
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 21:10:52 2022 -0300
    
        Updated install rule.
    
    commit 3003a6b1bbd1923aa663ad53a6caae0c62e6820b
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 20:48:59 2022 -0300
    
        Run make install
    
    commit 19541ff7b5e95523fe2c891791d3c594866fa771
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 20:34:37 2022 -0300
    
        Install libraries
    
    commit 172ad58e094b28ee1079759f9d4d89b55b11c1be
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 19:51:16 2022 -0300
    
        Update env in new rule
    
    commit afae88ed5629cb88bb107ee3c0793f520bec0a84
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 18:56:13 2022 -0300
    
        Fix lib value.
    
    commit 3f36d891ef153d88cfcbfb5ad83ba2eac024f79c
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 18:53:47 2022 -0300
    
        Set ld_library_path
    
    commit 3bda7f54c80912292e7e189eeae4310322274d81
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 16:16:40 2022 -0300
    
        Fix env path endings.
    
    commit f00438dca187a9307dadb44103e210eb88a2b298
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 15:03:06 2022 -0300
    
        Update env variables paths.
    
    commit 03c7b54128d9ef5ce7ea4ddc019af5c84ef168a7
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 14:30:38 2022 -0300
    
        Access workflow variable.
    
    commit 516b956027df0714bc85944dc28b9693d41fcf39
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Mon Sep 26 14:06:26 2022 -0300
    
        Set env variables on workflow.
    
    commit 9e2dde621bda085a1fa53ba624874b23502d22e5
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 18:13:48 2022 -0300
    
        Remove unit test folder.
    
    commit 6ea8c1cbf3e7ae5601057ba511aba139bbe41a77
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 18:05:15 2022 -0300
    
        Export vars in local command.
    
    commit 72e3a0f06a6d846de6e777e2bf83cb55e6561e97
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 17:42:11 2022 -0300
    
        Export env variables.
    
    commit 241a0529642b7a52a002517f773ff73e82f8496b
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 17:05:23 2022 -0300
    
        Create test data dir in makefile.
    
    commit 3e71e5cfb79e057e29d6718b0928852bb34ef98e
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 16:46:45 2022 -0300
    
        Updated gitignore.
    
    commit d839d58515681a36579cbd2f77c36e0b3a2e5cf8
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 16:45:49 2022 -0300
    
        Added advection gt data.
    
    commit c715b3bcd36c5acd6556b9350478758768ce5966
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 16:45:19 2022 -0300
    
        Added test results script.
    
    commit e5dc5ee7487bd9996251bf5e0e1c874627a8cea6
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 16:44:56 2022 -0300
    
        Added model test file.
    
    commit 71a45eaae0e578b580dfedafff51361bef916de0
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Sep 23 16:44:12 2022 -0300
    
        Update engine tests makefile.

---
+++ src/engine/tests/system/models_test.cpp
@@ -0,0 +1,65 @@
+/*****************************************************************************
+
+ This file is part of QSSModelInstance Solver.
+
+ QSSModelInstance Solver is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QSSModelInstance Solver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QSSModelInstance Solver.  If not, see
+ <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+#include <fstream>
+
+/// @addtogroup QssSolverSTests
+/// @{
+
+class SModelTests : public testing::TestWithParam<const char*> {
+};
+
+TEST_P(SModelTests, SimulateModel)
+{
+  const std::string NAME = GetParam();
+  std::cout << "Testing model: " << NAME << std::endl;
+  const std::string MODEL = " ./system/gt_data/" + NAME + "/" + NAME + ".mo";
+  const std::string FOLDER_CMD = "mkdir ./system/test_data/" + NAME;
+  const std::string MMOC = "../../mmoc/usr/bin/mmoc";
+  const std::string ARGS = " -o ./system/test_data/" + NAME + "/" + NAME + " -i ./system/test_data/packages ";
+  const std::string TEST_CMD = "./system/test_results.sh " + NAME;
+  const std::string SIM_CMD = "cd ./system/test_data/" + NAME + "; ./" + NAME;
+  const std::string RESULT_FILE = "./system/test_data/" + NAME + "/" + NAME + ".passed";
+  const std::string COMP_CMD = MMOC + ARGS + MODEL + " > ./system/test_data/" + NAME + "/" + NAME + ".log";
+  const std::string MAKE_CMD = "make -f ./system/test_data/" + NAME + "/" + NAME + ".makefile";
+
+  std::cout << "Setup data folders for " << NAME << std::endl;
+  std::system(FOLDER_CMD.c_str());
+  std::cout << "Testing model: " << NAME << std::endl;
+  std::system(COMP_CMD.c_str());
+  std::cout << "Running make for: " << NAME << std::endl;
+  std::system(MAKE_CMD.c_str());
+  std::cout << "Simulate model: " << NAME << std::endl;
+  std::system(SIM_CMD.c_str());
+  std::cout << "Check results for model: " << NAME << std::endl;
+  std::system(TEST_CMD.c_str());
+
+  std::ifstream result(RESULT_FILE.c_str());
+  EXPECT_TRUE(result.good());
+}
+
+const char* models[] = {"advection"};
+
+INSTANTIATE_TEST_SUITE_P(Models, SModelTests, testing::ValuesIn(models));
+
+/// @}
