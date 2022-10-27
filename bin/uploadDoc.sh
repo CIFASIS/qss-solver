417185d7b9f64d706d0d34bba5e1023d1e8bc707
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-10-27 10:57:59 -0300

    Update version.

--- bin/uploadDoc.sh
+++ bin/uploadDoc.sh
@@ -13,7 +13,7 @@
 #         NOTES: ---
 #        AUTHOR: Joaquin Fernandez, joaquin.f.fernandez@gmail.com
 #       PROJECT: QSS Solver
-#       VERSION: 4.0.0
+#       VERSION: 4.0.1
 #===================================================================================
 
 cd $MMOC_SRC/engine/usr/doc/html
