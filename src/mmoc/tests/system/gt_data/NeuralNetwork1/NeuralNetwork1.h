38a9636690e45d9e7b2285854b7375b6f0955fa6
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2022-10-27 11:19:02 -0300

    [iss-206] Squashed commit of the following:
    commit d78db5d8ab5a7f373af3f23ab2f82a845c307849
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Thu Oct 27 11:17:41 2022 -0300
    
        Fix pre commit hook.

--- src/mmoc/tests/system/gt_data/NeuralNetwork1/NeuralNetwork1.h
+++ src/mmoc/tests/system/gt_data/NeuralNetwork1/NeuralNetwork1.h
@@ -1,8 +1,8 @@
 // Model data access macro.
 
 #define MODEL_DATA_ACCESS(m) \
-  double* x = m->x;          \
-  double* d = m->d;          \
+  double* x = m->x; \
+  double* d = m->d; \
   double* a = m->alg;
 
 // Coeff multipliers definition.
@@ -15,32 +15,33 @@
 // Model Variables Macros
 
 // Macros definition for variable: C
-#define _idx_C(d1, d2) ((d1 - 1) * 20 + (d2 - 1))
-#define _C(d1, d2) __PAR__C[_idx_C(d1, d2)]
+#define _idx_C(d1,d2) ((d1-1)*20+(d2-1))
+#define _C(d1,d2) __PAR__C[_idx_C(d1,d2)]
 
 // Macros definition for variable: Cm
 #define _Cm __PAR__Cm
 
 // Macros definition for variable: Cn
-#define _idx_Cn(d1) ((d1 - 1))
+#define _idx_Cn(d1) ((d1-1))
 #define _Cn(d1) __PAR__Cn[_idx_Cn(d1)]
 
 // Macros definition for variable: EL
 #define _EL __PAR__EL
 
 // Macros definition for variable: Is
-#define _idx_Is(d1, coeff) ((d1 - 1))
-#define _state_idx_Is(d1, coeff) ((d1 - 1)) * 3 + coeff
-#define _Is(d1, coeff) a[_state_idx_Is(d1, coeff)]
+#define _idx_Is(d1,coeff) ((d1-1))
+#define _state_idx_Is(d1,coeff) ((d1-1))*3 + coeff
+#define _Is(d1,coeff) a[_state_idx_Is(d1,coeff)]
 #define _is_var_Is(idx) idx >= 0 && idx < 100
-#define _get_Is_idxs(idx) _d1 = (idx) + 1;
+#define _get_Is_idxs(idx)\
+		_d1 = (idx)+ 1;
 
 // Macros definition for variable: Islast
-#define _idx_Islast(d1) (300 + (d1 - 1))
+#define _idx_Islast(d1) (300+(d1-1))
 #define _Islast(d1) d[_idx_Islast(d1)]
 
 // Macros definition for variable: J
-#define _idx_J(d1) ((d1 - 1))
+#define _idx_J(d1) ((d1-1))
 #define _J(d1) __PAR__J[_idx_J(d1)]
 
 // Macros definition for variable: Jmean
@@ -53,44 +54,49 @@
 #define _Ni __PAR__Ni
 
 // Macros definition for variable: V
-#define _idx_V(d1, coeff) ((d1 - 1))
-#define _state_idx_V(d1, coeff) ((d1 - 1)) * 3 + coeff
-#define _V(d1, coeff) x[_state_idx_V(d1, coeff)] * COEFF_MULTIPLIER(coeff)
-#define _init_V(d1, coeff) x[_state_idx_V(d1, coeff)]
-#define _eval_V(d1, coeff) ((d1 - 1))
+#define _idx_V(d1,coeff) ((d1-1))
+#define _state_idx_V(d1,coeff) ((d1-1))*3 + coeff
+#define _V(d1,coeff) x[_state_idx_V(d1,coeff)] * COEFF_MULTIPLIER(coeff)
+#define _init_V(d1,coeff) x[_state_idx_V(d1,coeff)]
+#define _eval_V(d1,coeff) ((d1-1))
 #define _is_var_V(idx) idx >= 0 && idx < 100
-#define _get_V_idxs(idx) _d1 = (idx) + 1;
-#define _eval_dep_V(d1, coeff) dx[_state_idx_V(d1, coeff)]
+#define _get_V_idxs(idx)\
+		_d1 = (idx)+ 1;
+#define _eval_dep_V(d1,coeff) dx[_state_idx_V(d1,coeff)]
 
 // Macros definition for variable: Vr
 #define _Vr __PAR__Vr
 
 // Macros definition for variable: _event_1
-#define _idx_event_1(d1) ((d1 - 1))
-#define _eval_event_1(d1) ((d1 - 1))
+#define _idx_event_1(d1) ((d1-1))
+#define _eval_event_1(d1) ((d1-1))
 #define _is_var_event_1(idx) idx >= 0 && idx < 100
-#define _get_event_1_idxs(idx) _d1 = (idx) + 1;
+#define _get_event_1_idxs(idx)\
+		_d1 = (idx)+ 1;
 
 // Macros definition for variable: _event_2
-#define _idx_event_2(d1) (100 + (d1 - 1))
-#define _eval_event_2(d1) (100 + (d1 - 1))
+#define _idx_event_2(d1) (100+(d1-1))
+#define _eval_event_2(d1) (100+(d1-1))
 #define _is_var_event_2(idx) idx >= 100 && idx < 200
-#define _get_event_2_idxs(idx) _d1 = (idx - 100) + 1;
+#define _get_event_2_idxs(idx)\
+		_d1 = (idx-100)+ 1;
 
 // Macros definition for variable: _event_3
-#define _idx_event_3(d1) (200 + (d1 - 1))
-#define _eval_event_3(d1) (200 + (d1 - 1))
+#define _idx_event_3(d1) (200+(d1-1))
+#define _eval_event_3(d1) (200+(d1-1))
 #define _is_var_event_3(idx) idx >= 200 && idx < 300
-#define _get_event_3_idxs(idx) _d1 = (idx - 200) + 1;
+#define _get_event_3_idxs(idx)\
+		_d1 = (idx-200)+ 1;
 
 // Macros definition for variable: _out_exp_1
-#define _idx_out_exp_1(d1) ((d1 - 1))
-#define _eval_out_exp_1(d1) ((d1 - 1))
+#define _idx_out_exp_1(d1) ((d1-1))
+#define _eval_out_exp_1(d1) ((d1-1))
 #define _is_var_out_exp_1(idx) idx >= 0 && idx < 10
-#define _get_out_exp_1_idxs(idx) _d1 = (idx) + 1;
+#define _get_out_exp_1_idxs(idx)\
+		_d1 = (idx)+ 1;
 
 // Macros definition for variable: active
-#define _idx_active(d1) (200 + (d1 - 1))
+#define _idx_active(d1) (200+(d1-1))
 #define _active(d1) d[_idx_active(d1)]
 
 // Macros definition for variable: aux
@@ -127,15 +133,15 @@
 #define _theta __PAR__theta
 
 // Macros definition for variable: tlast
-#define _idx_tlast(d1) (400 + (d1 - 1))
+#define _idx_tlast(d1) (400+(d1-1))
 #define _tlast(d1) d[_idx_tlast(d1)]
 
 // Macros definition for variable: tnext
-#define _idx_tnext(d1) ((d1 - 1))
+#define _idx_tnext(d1) ((d1-1))
 #define _tnext(d1) d[_idx_tnext(d1)]
 
 // Macros definition for variable: tnextr
-#define _idx_tnextr(d1) (100 + (d1 - 1))
+#define _idx_tnextr(d1) (100+(d1-1))
 #define _tnextr(d1) d[_idx_tnextr(d1)]
 
 // Macros definition for variable: vbg
@@ -143,6 +149,7 @@
 
 // Macros definition for variable: vext
 #define _vext __PAR__vext
+
 
 // Model Parameters Declaration
 
@@ -194,56 +201,62 @@ double __PAR__vext;
 // Derivative Equations Macros
 
 // Macros for equation: 1
-#define _apply_usage_eq_1(_d1) i = _d1;
-#define _get_eq_1_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;        \
-  var = _idx_V(_rg_d1, 0);
+#define _apply_usage_eq_1(_d1) \
+	i = _d1;
+#define _get_eq_1_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_V(_rg_d1,0);
 
 // Algebraic Equations Macros
 
 // Macros for algebraic equation: 1
-#define _apply_usage_alg_eq_1(_d1) i = _d1;
-#define _get_alg_eq_1_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;            \
-  var = _idx_Is(_rg_d1, 0);
+#define _apply_usage_alg_eq_1(_d1) \
+	i = _d1;
+#define _get_alg_eq_1_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_Is(_rg_d1,0);
 
 // Event Macros
 
 // Macros for event: 1
-#define _apply_usage_event_1(_d1) i = _d1;
-#define _get_event_1_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;           \
-  var = _idx_event_1(_rg_d1);
+#define _apply_usage_event_1(_d1) \
+	i = _d1;
+#define _get_event_1_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_event_1(_rg_d1);
 // Macros for event: 2
-#define _apply_usage_event_2(_d1) i = _d1;
-#define _get_event_2_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;           \
-  var = _idx_event_2(_rg_d1);
+#define _apply_usage_event_2(_d1) \
+	i = _d1;
+#define _get_event_2_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_event_2(_rg_d1);
 // Macros for event: 3
-#define _apply_usage_event_3(_d1) i = _d1;
-#define _get_event_3_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;           \
-  var = _idx_event_3(_rg_d1);
+#define _apply_usage_event_3(_d1) \
+	i = _d1;
+#define _get_event_3_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_event_3(_rg_d1);
 #define _zc(coeff) zc[coeff]
 
 // Output Equations Macros
 
 // Macros for output equation: 1
-#define _apply_usage_out_exp_1(_d1) _d1 = _d1;
-#define _get_out_exp_1_var_idxs(row, var) \
-  _rg_d1 = 0 + (row - 1) + 1;             \
-  var = _idx_out_exp_1(_rg_d1);
+#define _apply_usage_out_exp_1(_d1) \
+	_d1 = _d1;
+#define _get_out_exp_1_var_idxs(row, var)\
+	_rg_d1 = 0 + (row-1)+ 1;\
+	var = _idx_out_exp_1(_rg_d1);
 #define _out out[0]
 
-// Jacobian Macros definition.
-#define _assign_jac(r, val)                                    \
-  col_t = dvdx->df_dx_t->size[r] + dvdx->df_dx_t->index[r][0]; \
-  dvdx->df_dx_t->index[r][0]++;                                \
-  jac[col_t] = val;
-#define _c_index(i) (i - 1)
+// Jacobian Macros definition. 
+#define _assign_jac(r, val) \
+    col_t = dvdx->df_dx_t->size[r] + dvdx->df_dx_t->index[r][0]; \
+    dvdx->df_dx_t->index[r][0]++; \
+    jac[col_t] = val;
+#define _c_index(i) (i-1)
 
 #define _time t
 
-// Derivative Macros definition.
+// Derivative Macros definition. 
 // Derivative definition for variable: V
-#define _der_V(d1, coeff) dx[coeff + 1]
+#define _der_V(d1,coeff) dx[coeff+1]
