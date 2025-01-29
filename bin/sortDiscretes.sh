b3bc65d02e005f57ed5e030424c4731790c4ea5f
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2025-01-29 10:23:15 -0300

    Update version

--- bin/sortDiscretes.sh
+++ bin/sortDiscretes.sh
@@ -15,7 +15,7 @@
 #         NOTES: ---
 #        AUTHOR: Joaquin Fernandez, joaquin.f.fernandez@gmail.com
 #       PROJECT: QSS Solver
-#       VERSION: 4.5.2
+#       VERSION: 4.5.3
 #===================================================================================
 
 if test -n "$(find . -maxdepth 1 -name '*-discrete-*.dat' -print -quit)"; then
