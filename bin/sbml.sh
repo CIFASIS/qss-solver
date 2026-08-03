5a6c864c60cffded7974fee4ff5f407d12c0b4ed
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2026-08-03 10:06:43 -0300

    Updated version.

--- bin/sbml.sh
+++ bin/sbml.sh
@@ -16,7 +16,7 @@
 #         NOTES: --- 
 #        AUTHOR: Joaquin Fernandez, joaquin.f.fernandez@gmail.com
 #       PROJECT: QSS Solver
-#       VERSION: 6.2.0
+#       VERSION: 6.2.1
 #===================================================================================
 
 if [ "$#" -ne 2 ]; then
--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -15,7 +15,7 @@
 #         NOTES: --- 
 #        AUTHOR: Joaquin Fernandez, joaquin.f.fernandez@gmail.com
 #       PROJECT: QSS Solver
-#       VERSION: 6.2.0
+#       VERSION: 6.2.1
 #===================================================================================
 
 set -euo pipefail
